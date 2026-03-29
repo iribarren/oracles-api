0dc6244e17f036ef86698fde4e22144b8e46415c
Aritz Iribarren Galarza <iribarren.aritz@gmail.com>
2026-03-30 00:44:30 +0200

    fix

--- src/Controller/Admin/GameSessionCrudController.php
+++ src/Controller/Admin/GameSessionCrudController.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Entity\GameSession;
+use App\Enum\GamePhase;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
@@ -27,7 +29,11 @@ class GameSessionCrudController extends AbstractCrudController
         yield TextField::new('character_name', 'Character Name');
         yield TextField::new('genre');
         yield TextField::new('epoch');
-        yield TextField::new('current_phase', 'Current Phase');
+        yield ChoiceField::new('current_phase', 'Current Phase')
+            ->setChoices(array_combine(
+                array_map(fn (GamePhase $p) => $p->value, GamePhase::cases()),
+                GamePhase::cases(),
+            ));
         yield IntegerField::new('overcome_score', 'Overcome Score');
         yield DateTimeField::new('created_at', 'Created At')->onlyOnIndex();
         yield DateTimeField::new('updated_at', 'Updated At')->onlyOnIndex();
